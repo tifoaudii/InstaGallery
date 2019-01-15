78067f4225b8b72b5bae3ac644ea7bd849f225ca
tifoaudy <165150200111168@mail.ub.ac.id>
2019-01-15 21:07:57 +0700

    generating project

---
+++ Insta Gallery/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Insta Gallery
+//
+//  Created by Tifo Audi Alif Putra on 15/01/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
