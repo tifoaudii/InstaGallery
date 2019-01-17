620865e84dc497fe56f8987a2dcd6eeb892e706c
tifoaudy <165150200111168@mail.ub.ac.id>
2019-01-17 10:00:41 +0700

    create dummy data

---
+++ Insta Gallery/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  Insta Gallery
+//
+//  Created by Tifo Audi Alif Putra on 17/01/19.
+//  Copyright © 2019 BCC FILKOM. All rights reserved.
+//
+
+import UIKit
+
+let laptop = UIImage(named: "1")!
+let coffee = UIImage(named: "2")!
+let phone = UIImage(named: "3")!
+let camera = UIImage(named: "4")!
+let cup = UIImage(named: "5")!
+
+let categories = ["LAPTOP","COFFEE","PHONE","CAMERA","CUP"]
+let images :[UIImage] = [laptop,coffee,phone,camera,cup]
